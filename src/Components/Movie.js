501ec7e21d50693d8ba4450a33dbf38e1f36ff48
Lix311 <alexsterrantino@gmail.com>
2020-09-12 14:02:38 -0400

    cards working

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
+import {Card} from 'react-bootstrap'
+import {withRouter} from 'react-router-dom'
 
 class Movie extends Component {
     state = {  }
+
+    getGenre = (genreids) => {
+        let genreArray = []
+        genreids.map(id => {
+            if (id === 28){genreArray.push("Action" + ' ')}
+            if (id === 12){genreArray.push("Adventure" + ' ')}
+            if (id === 16){genreArray.push("Animation"+ ' ')}
+            if (id === 35){genreArray.push("Comedy"+ ' ') }
+            if (id === 80){genreArray.push("Crime"+ ' ')}
+            if (id === 99){genreArray.push("Documentary"+ ' ')}
+            if (id === 18){genreArray.push("Drama"+ ' ') }
+            if (id === 10751){genreArray.push("Family"+ ' ')} 
+            if (id === 14){genreArray.push("Fantasy"+ ' ') }
+            if (id === 36){genreArray.push("History"+ ' ') }
+            if (id === 27){genreArray.push("Horror"+ ' ') }
+            if (id === 10402){genreArray.push("Music"+ ' ')} 
+            if (id === 9648){genreArray.push("Mystery"+ ' ')} 
+            if (id === 10749){genreArray.push("Romance"+ ' ')} 
+            if (id === 878){genreArray.push("Science Fiction"+ ' ')}
+            if (id === 10770){genreArray.push("TV Movie"+ ' ') }
+            if (id === 53){genreArray.push("Thriller"+ ' ') }
+            if (id === 10752){genreArray.push("War"+ ' ') }
+            if (id === 37){genreArray.push("Western"+ ' ')}
+            
+        })
+
+        return genreArray
+       
+    }
+
+    clickHandler = () => {
+        this.props.history.push('/home/' + this.props.movie.id)
+    }
+    
     render() { 
         return (  
-        <h2>{this.props.movie.title}</h2>
+        <div className='movie-card'>
+            {console.log(this.props.movie)}
+            <img onClick={this.clickHandler} className='movie-img' src={'http://image.tmdb.org/t/p/w185//' + this.props.movie.poster_path}/>
+            <Card>
+    <Card.Body>
+        <Card.Title>{this.props.movie.title}</Card.Title>
+      <Card.Text>
+          {'Vote Average: ' + this.props.movie.vote_average}
+          <br/>
+          {'Release Date: ' + this.props.movie.release_date}
+
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+        <small>{this.getGenre(this.props.movie.genre_ids)}</small>
+    </Card.Footer>
+  </Card>
+        </div>
+
         );
     }
 }
  
-export default Movie;
+export default withRouter(Movie);
--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom'
 import {Jumbotron, Container, Row, Col, Image, Button} from 'react-bootstrap'
 import '../App.css';
 
 class HomeContainer extends Component {
     state = {  }
+    
+    clickHandler = () => {
+        this.props.history.push('/home')
+    }
+
     render() { 
         // changed emails for gits commits to work
         return (  
             <div>
                 <Image src="assets/theatre.jpg" className='home-img'/>
-                <button className='home-btn'>Enter</button>
+                <button onClick={this.clickHandler} className='home-btn'>Enter</button>
                 
             </div>
         );
     }
 }
  
-export default HomeContainer;
+export default withRouter(HomeContainer);
