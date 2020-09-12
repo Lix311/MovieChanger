455a99148dd2618f37a8fe3a20ade0558f18109b
Lix311 <alexsterrantino@gmail.com>
2020-09-12 00:13:20 -0400

    homepage done and api works

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -5,10 +5,12 @@ import '../App.css';
 class HomeContainer extends Component {
     state = {  }
     render() { 
+        // changed emails for gits commits to work
         return (  
             <div>
                 <Image src="assets/theatre.jpg" className='home-img'/>
                 <button className='home-btn'>Enter</button>
+                
             </div>
         );
     }
